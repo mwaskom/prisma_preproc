82248b6fe8767c66eda9f10a05c3316d77c8c58c
Michael Waskom <mwaskom@nyu.edu>
2017-06-26 12:02:58 -0400

    Build out anatomical workflow through T2w->T1w registration

--- prisma_anat.py
+++ prisma_anat.py
@@ -1,11 +1,12 @@
 import os
-import nipype
 from nipype import (Workflow, Node, MapNode,
                     Function, IdentityInterface, SelectFiles, DataSink)
 from nipype.interfaces import fsl, freesurfer as fs, utility
 
 
 data_dir = os.environ["SUBJECTS_DIR"]
+
+# --- Define data inputs
 
 subject_source = Node(IdentityInterface(["subject"]),
                       name="subject_source",
@@ -18,6 +19,10 @@ file_input = Node(SelectFiles(templates,
                               base_directory=data_dir),
                   "file_input")
 
+reorient_t1w = MapNode(fsl.Reorient2Std(), "in_file", "reorient_t1w")
+reorient_t2w = MapNode(fsl.Reorient2Std(), "in_file", "reorient_t2w")
+
+# --- Register and average the repetitions of the anatomical volumes
 
 register_t1w = Node(fs.RobustTemplate(initial_timepoint=1,
                                       fixed_timepoint=True,
@@ -25,33 +30,93 @@ register_t1w = Node(fs.RobustTemplate(initial_timepoint=1,
                                       auto_detect_sensitivity=True,
                                       intensity_scaling=True,
                                       no_iteration=True),
-                     "register_t1w")
+                    "register_t1w")
 
 register_t2w = register_t1w.clone("register_t2w")
 
 register_t1w.inputs.out_file = "T1w.nii.gz"
 register_t2w.inputs.out_file = "T2w.nii.gz"
 
-robust_args = "--cost ROBENT --entradius 2 --entcorrection"
-register_between = Node(fs.RobustRegister(est_int_scale=True,
-                                          auto_sens=True,
-                                          args=robust_args,
-                                          registered_file="T2w.nii.gz"),
+
+# --- Register the T1w volume to the MNI template
+
+mni_template = fsl.Info.standard_image("MNI152_T1_2mm.nii.gz")
+mni_mask = fsl.Info.standard_image("MNI152_T1_2mm_brain_mask.nii.gz")
+
+normalize_linear = Node(fsl.FLIRT(dof=12,
+                                  reference=mni_template,
+                                  interp="spline"),
+                        "normalize_linear")
+
+normalize_nonlinear = Node(fsl.FNIRT(config_file="T1_2_MNI152_2mm",
+                                     ref_file=mni_template,
+                                     refmask_file=mni_mask),
+                           "normalize_nonlinear")
+
+# --- Invert the warpfield and bring the MNI mask into native space
+
+invert_warp = Node(fsl.InvWarp(), "invert_warp")
+
+warp_mask = Node(fsl.ApplyWarp(in_file=mni_mask, interp="nn"),
+                 "warp_mask")
+
+fill_mask = Node(fsl.ImageMaths(op_string="fillh"), "fill_mask")
+
+dilate_mask = Node(fsl.DilateImage(operation="max"), "dilate_mask")
+
+# --- Register the T2w to the T1w volume
+
+# Technically better but takes forever, and given smoothing of
+# bias field the difference is likely of no consequence
+# robust_args = "--cost ROBENT --entradius 2 --entcorrection"
+# register_between = Node(fs.RobustRegister(est_int_scale=True,
+#                                           auto_sens=True,
+#                                           args=robust_args,
+#                                           registered_file="T2w.nii.gz"),
+#                         "register_between")
+
+register_between = Node(fsl.FLIRT(dof=6, interp="spline"),
                         "register_between")
+
+# --- Build the workflow
 
 workflow = Workflow(name="prisma_anat", base_dir="nipype_cache")
 
 workflow.connect([
     (subject_source, file_input,
         [("subject", "subject")]),
-    (file_input, register_t1w,
-        [("t1w_files", "in_files")]),
-    (file_input, register_t2w,
-        [("t2w_files", "in_files")]),
+    (file_input, reorient_t1w,
+        [("t1w_files", "in_file")]),
+    (file_input, reorient_t2w,
+        [("t2w_files", "in_file")]),
+    (reorient_t1w, register_t1w,
+        [("out_file", "in_files")]),
+    (reorient_t2w, register_t2w,
+        [("out_file", "in_files")]),
+    (register_t1w, normalize_linear,
+        [("out_file", "in_file")]),
+    (register_t1w, normalize_nonlinear,
+        [("out_file", "in_file")]),
+    (normalize_linear, normalize_nonlinear,
+        [("out_matrix_file", "affine_file")]),
+    (register_t1w, invert_warp,
+        [("out_file", "reference")]),
+    (normalize_nonlinear, invert_warp,
+        [("fieldcoeff_file", "warp")]),
+    (register_t1w, warp_mask,
+        [("out_file", "ref_file")]),
+    (invert_warp, warp_mask,
+        [("inverse_warp", "field_file")]),
+    (warp_mask, fill_mask,
+        [("out_file", "in_file")]),
+    (fill_mask, dilate_mask,
+        [("out_file", "in_file")]),
     (register_t1w, register_between,
-        [("out_file", "target_file")]),
+        [("out_file", "reference")]),
     (register_t2w, register_between,
-        [("out_file", "source_file")]),
+        [("out_file", "in_file")]),
+    (dilate_mask, register_between,
+        [("out_file", "ref_weight")]),
 ])
 
 
